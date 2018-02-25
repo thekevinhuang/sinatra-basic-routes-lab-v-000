94b1d4f0c9b623e422acf1f0d0a743740ba0ecce
Kevin Huang <yellow.khaos@gmail.com>
2018-02-25 19:46:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Kevin"
+  end
+
+  get '/hometown' do
+    "My hometown is Princeton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Your Song"
+  end
 end
