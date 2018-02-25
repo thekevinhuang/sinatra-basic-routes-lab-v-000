2bad9f985e1d1d8a106b0bbece5aae5355494ebe
Kevin Huang <yellow.khaos@gmail.com>
2018-02-25 19:46:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+
+  get '/name' do
+    "My name is Kevin"
+  end
+
+  get '/hometown' do
+    "My hometown is Princeton"
+  end
+
+  get 'favorite-song' do
+    "My favorite song is Your Song"
+  end
 end
